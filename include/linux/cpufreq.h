33d30d1c771f0c2596dac46f3ac141a772e93046
yank555-lu <yank555.lu@gmail.com>
2014-12-28 18:10:45 +0100

    cpufreq: add yankactive governor
    Interactive governor with, fow now :
    
    - hispeed_freq defaulting to lower than policy_max (1.73GHz)
    - target_loads increased to 95
    - min_sampling time reduced to 20ms
    - above_hispeed_delay increased to 80ms
    
    Change-Id: Ic5b5abd695808fa69f15504c0b8e911c95dfab0f

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -471,6 +471,9 @@ extern struct cpufreq_governor cpufreq_gov_cafactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 /*********************************************************************
