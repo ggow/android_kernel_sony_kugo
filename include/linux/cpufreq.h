b47cf846d9034d7580c26eb9a70e045801e75f32
Tkkg1994 <luca.grifo@outlook.com>
2015-12-25 19:03:22 +0100

    cpufreq: add smartmax and smartass2 governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -451,6 +451,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
 #endif
 
 /*********************************************************************
