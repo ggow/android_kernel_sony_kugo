415171e25d579bcbe44df74e805273c71c995b17
ggow <dontouchmyfood@gmail.com>
2016-12-24 08:11:41 +0000

    Add build script

---
+++ build_it4_kugo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### GCC 4.9.x
+### I'm using UBERTC https://bitbucket.org/UBERTC/aarch64-linux-android-4.9-kernel
+
+export ARCH=arm64
+export PATH=/mnt/data/android/Xperia/aarch64-linux-android-4.9-kernel/bin/:$PATH
+
+### See prefix of file names in the toolchain's bin directory
+export CROSS_COMPILE=aarch64-linux-android-
+
+export KBUILD_DIFFCONFIG=kugo_diffconfig
+make msm-perf_defconfig O=/mnt/out/kugo_kernel
+time make -j8 O=/mnt/out/kugo_kernel
